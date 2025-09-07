8f2eed7d95ae687ac20a37169c0606011fda85f6
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-09-07 23:08:22 +0700

    navbar

--- src/app/home/partials/navbar.tsx
+++ src/app/home/partials/navbar.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>
-        
+    <div className='flex justify-between p-4'>
+      <h1 className='text-2xl font-bold'>Healthy Shop</h1>
+      <ul className='flex items-center gap-4'>
+        <li className='cursor-pointer'>Home</li>
+        <li className='cursor-pointer'>About</li>
+        <li className='cursor-pointer'>Contact</li>
+      </ul>
     </div>
-    
-)}
+  );
+};
 
-export default Navbar
+export default Navbar;
